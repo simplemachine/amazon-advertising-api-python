02d187a10f86e268bcadc88b49cac88364d07365
Jason Fertel <jason.fertel@gmail.com>
2017-10-12 15:16:57 -0400

    update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import amazon_advertising_api.versions as aa_versions
 
 setup(
     name='amazon_advertising',
-    packages=['amazon_advertising'],
+    packages=['amazon_advertising_api'],
     version=aa_versions.versions['application_version'],
     description='Unofficial Amazon Sponsored Products Python client library.',
     url='https://github.com/sguermond/amazon-advertising-api-python')
